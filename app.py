1f5356be2abf98e93c43f8994a2c9d4d1aa1ad17
jtgama <jhonatanralla@hotmail.com>
2019-10-23 03:29:11 -0500

    primero

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_mysqldb import MySQL
 
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost' 
-app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_HOST'] = 'db-server-proyectop.mysql.database.azure.com' 
+app.config['MYSQL_USER'] = 'jtgama@db-server-proyectop'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'participantesp'
 mysql = MySQL(app)
@@ -74,5 +74,3 @@ def delete_partici(id):
    flash('Participante Eliminado Exitosamente')
    return redirect(url_for('Index')) 
 
-if __name__ == '__main__':
-    app.run(port=3000, debug= True)
