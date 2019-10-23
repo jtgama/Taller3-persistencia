6e62f95eaf5e53d097e7cd587b457034393f6c8a
jtgama <jhonatanralla@hotmail.com>
2019-10-23 03:31:31 -0500

    primerop

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'db-server-proyectop.mysql.database.azure.com' 
 app.config['MYSQL_USER'] = 'jtgama@db-server-proyectop'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '@Jt3028@'
 app.config['MYSQL_DB'] = 'participantesp'
 mysql = MySQL(app)
 
